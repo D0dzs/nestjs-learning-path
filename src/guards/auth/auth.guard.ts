0ae2d9a332a408ecb53ec5c0ea27afc043650787
Dodzs <gameeaters9@gmail.com>
2024-10-17 17:43:41 +0200

    feat(guards): tokens in db, scuffed validation v2

--- src/guards/auth/auth.guard.ts
+++ src/guards/auth/auth.guard.ts
@@ -1,13 +1,39 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(ctx: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-    const req = ctx.switchToHttp().getRequest();
-    const authToken = req.headers['authorization'].split(' ')[1];
+  constructor(private readonly prisma: PrismaService) {}
 
-    if (authToken === 'super-secret-token-to-test') return true;
-    else new UnauthorizedException();
+  async canActivate(ctx: ExecutionContext): Promise<boolean> {
+    const req = ctx.switchToHttp().getRequest();
+    const authToken = req.headers['authorization']?.split(' ')[1];
+    if (!authToken) throw new UnauthorizedException();
+
+    const isValidToken = await this.prisma.validTokens.findUnique({
+      where: {
+        token: authToken,
+      },
+    });
+
+    // It's so scuffed please don't judge me ^-^
+    // TODO: A better way to to this... D:
+    if (!isValidToken) throw new UnauthorizedException('Invalid Token...');
+    if (new Date() >= new Date(isValidToken.expiresAt)) throw new UnauthorizedException('Expired Token...');
+    else {
+      const res = await this.prisma.validTokens.update({
+        where: {
+          token: authToken,
+        },
+        data: {
+          expiresAt: new Date(new Date().getTime() + 15 * 60000),
+        },
+      });
+
+      if (!res) throw new InternalServerErrorException();
+    }
+
+    // line:9 - Promise<boolean>
+    return true;
   }
 }
